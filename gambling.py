f5dbe6ed5b5a35e4b2cf2003a81418cdfad9d645
zyang <yang@fettle.fr>
2016-05-18 14:00:22 +0200

    Added analyse functions

--- gambling.py
+++ gambling.py
@@ -43,6 +43,22 @@ def play(times, winProbability, strategy, initBet, balance):
         balances += [balance]
     return (i, balance, balances)
 
+def getMean(list):
+    return sum(list)/len(list)
+
+def getVariance(list):
+    mean = getMean(list)
+    v = []
+    for i in list:
+        v += [pow(i - mean, 2)]
+    return sum(v)/len(list)
+
+def getLoseTimes(times, winProbability, strategy, initBet, balance):
+    ltime = []
+    for i in range(times):
+        ltime += [play(times, winProbability, strategy, initBet, balance)[0]]
+    return ltime
+
 def getAverageLoseTime(times, winProbability, strategy, initBet, balance):
     loseAt = 0
     for i in range(times):
@@ -50,9 +66,15 @@ def getAverageLoseTime(times, winProbability, strategy, initBet, balance):
     return loseAt / times
 
 def getAverageMaximumBalance(times, winProbability, strategy, initBet, balance):
+    maxBalances = []
+    for i in range(times):
+        maxBalances += [getMaximumBalance(play(times, winProbability, strategy, initBet, balance))[2]]
+    return maxBalances    
+
+def getAverageMaximumBalance(times, winProbability, strategy, initBet, balance):
     maxBalance = 0
     for i in range(times):
-        maxBalance += getMaximumBalance(play(times, winProbability, strategy, initBet, balance)[2])
+        maxBalance += [getMaximumBalance(play(times, winProbability, strategy, initBet, balance))[2]]
     return maxBalance / times
 
 def getMaximumBalance(balances):
@@ -62,16 +84,19 @@ def getMaximumRatio(balances, initBalance):
     return max(balances) / initBalance
     
 def analyseRatioAndBalance(winProbability, strategy, begin, stepping, numberOfSteps, times = 100000):
-    ratios = []
+    ratios = {}
     balance = begin
     for i in range(numberOfSteps):
-        ratios += [getAverageMaximumBalance(times, winProbability, strategy, 100, balance) / balance]
+        ratios[balance] = getAverageMaximumBalance(times, winProbability, strategy, 100, balance) / balance
         balance *= stepping
     return ratios
 
-def outputToCsv(data, path, x = "time", y = "balance"):
+def outputListToCsv(data, path, x = "time", y = "balance"):
     fp = open(path, "w+")
     fp.write(x + "," + y + "\n")
     for i in range(len(data)):
         fp.write(str(i + 1) + "," + str(data[i]) + "\n")
     fp.close()
+
+def outputDictionaryToCsc(data, path, x = "balance", y = "ratio"):
+    pass
